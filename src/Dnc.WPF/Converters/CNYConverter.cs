56ef03a119a171f49628a5884f62322feb09c45e
Zj <519564415@qq.com>
2019-03-28 09:51:52 +0800

    Wpf Converters:CNYConverter

---
+++ src/Dnc.WPF/Converters/CNYConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace Dnc.WPF.Converters
+{
+    [ValueConversion(typeof(decimal), typeof(string))]
+    public class CNYConverter
+        : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var cny = (decimal)value;
+
+            return $"￥{cny.ToString("#.00")}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return 0m;
+
+            if (decimal.TryParse(value.ToString(), out var cny))
+                return cny;
+
+            return 0m;
+        }
+    }
+}
