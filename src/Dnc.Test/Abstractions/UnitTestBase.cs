e9e717993f2312e362156b63b0672e1279c7859c
Zj <519564415@qq.com>
2019-05-08 16:39:04 +0800

    unittestbase&integrationtestbase

---
+++ src/Dnc.Test/Abstractions/UnitTestBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace Dnc.Test
+{
+    public class UnitTestBase
+    {
+        protected ITestOutputHelper _output;
+        public UnitTestBase(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+    }
+}
