eaa3971648b6f0e2713019dd948977d1bdd6f702
Zj <519564415@qq.com>
2019-05-11 11:08:28 +0800

    queryparameters

--- src/Dnc.Core/SeedWork/Biz/AdminAggregate/SysUserLoginLog.cs
+++ src/Dnc.Core/SeedWork/Biz/AdminAggregate/SysUserLoginLog.cs
@@ -5,10 +5,22 @@ using System.Text;
 namespace Dnc.Seedwork
 {
     public class SysUserLoginLog
-        :Entity
+        : Entity
     {
+        public SysUserLoginLog()
+        {
+            LoginTime = DateTime.Now;
+        }
+        public SysUserLoginLog(long userId,string msg,string ip=null)
+            :this()
+        {
+            UserId = userId;
+            Message = msg;
+            IPAddress = ip;
+        }
         public long UserId { get; set; }
         public DateTime LoginTime { get; set; }
         public string IPAddress { get; set; }
+        public string Message { get; set; }
     }
 }
--- src/Dnc.Core/SeedWork/Biz/AdminAggregate/SysUserManager.cs
+++ src/Dnc.Core/SeedWork/Biz/AdminAggregate/SysUserManager.cs
@@ -21,14 +21,27 @@ namespace Dnc.Seedwork
         public async Task<(bool Success, string Msg)> SignInAsync(string name, string pwd)
         {
             var user = await _ctx.SysUser.SingleOrDefaultAsync(u => u.UName.Equals(name, StringComparison.OrdinalIgnoreCase));
+            string message = string.Empty;
             if (user == null)
-                return (false, "该账号尚未注册");
+            {
+                message = "该账号尚未注册";
+                await AddLoginLog(user.Id, message);
+                return (false, message);
+            }
 
             if (user.IsLocked && user.AllowLoginTime >= DateTime.Now)
-                return (false, $"该账号已因登录错误次数过多已被锁定,锁定剩余时间{(user.AllowLoginTime - DateTime.Now).Value.TotalSeconds}秒");
+            {
+                message = $"该账号已因登录错误次数过多已被锁定,锁定剩余时间{(user.AllowLoginTime - DateTime.Now).Value.TotalSeconds}秒";
+                await AddLoginLog(user.Id, message);
+                return (false, message);
+            }
 
             if (user.DataStatus == DataStatusEnum.Deleted || user.DataStatus == DataStatusEnum.UnAudited)
-                return (false, $"该账号因违规已被管理员冻结或者删除");
+            {
+                message = "该账号因违规已被管理员冻结或者删除";
+                await AddLoginLog(user.Id, message);
+                return (false, message);
+            }
 
             if (!user.Pwd.Equals(pwd))
             {
@@ -38,18 +51,29 @@ namespace Dnc.Seedwork
                     user.AllowLoginTime = DateTime.Now.AddMinutes(LockMinutes);
                     user.IsLocked = true;
                 }
-                return (false, $"该账号已因登录错误次数过多已被锁定,将被锁定{LockMinutes * 60}秒");
+                message = $"该账号已因登录错误次数过多已被锁定,将被锁定{LockMinutes * 60}秒";
+                await AddLoginLog(user.Id, message);
+                return (false, message);
             }
 
             user.LoginErrNums = 0;
             user.AllowLoginTime = null;
             user.IsLocked = false;
-            return (true, "登录成功");
+            user.LastLoginTime = DateTime.Now;
+            user.LastLoginIP = "";
+
+            message = "登录成功";
+            await AddLoginLog(user.Id, message, save: false);
+
+            var token = new SysUserToken(user.Id);
+            await _ctx.SysUserToken.AddAsync(token);
+            await _ctx.SaveChangesAsync();
+            return (true, message);
         }
 
         public async Task<(bool Success, string Msg)> SignUpAsync(string name)
         {
-            var user =await _ctx.SysUser.SingleOrDefaultAsync(u => u.UName.Equals(name, StringComparison.OrdinalIgnoreCase));
+            var user = await _ctx.SysUser.SingleOrDefaultAsync(u => u.UName.Equals(name, StringComparison.OrdinalIgnoreCase));
             if (user != null)
                 return (false, "该账号已被注册");
             var usr = new SysUser()
@@ -60,6 +84,15 @@ namespace Dnc.Seedwork
             await _ctx.SaveChangesAsync();
 
             return (true, "注册成功");
+        }
+
+        private async Task AddLoginLog(long userId, string msg, string ip = null, bool save = true)
+        {
+            var userLoginLog = new SysUserLoginLog(userId, msg, ip);
+            await _ctx.SysUserLoginLog.AddAsync(userLoginLog);
+
+            if (save)
+                await _ctx.SaveChangesAsync();
         }
     }
 }
