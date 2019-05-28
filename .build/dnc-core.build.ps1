21ae14d8bc1b37cd14920b2794c887af2f3b30a8
Zj <519564415@qq.com>
2019-05-28 16:33:40 +0800

    1.1.0.16

--- .build/dnc-core.build.ps1
+++ .build/dnc-core.build.ps1
@@ -41,6 +41,13 @@ $nugetPath=join-path $packPath 'bin/release/'
 cd $nugetPath
 dotnet nuget push Dnc.Events.$version.nupkg -k oy2jhggtncepirf5it4qtbuyrppwfhke7mi4tswqf4mbf4 -s https://api.nuget.org/v3/index.json
 
+$packPath=join-path $workspace 'src/Dnc.CQRS'
+cd $packPath
+dotnet pack -c release -o bin/release
+$nugetPath=join-path $packPath 'bin/release/'
+cd $nugetPath
+dotnet nuget push Dnc.CQRS.$version.nupkg -k oy2jhggtncepirf5it4qtbuyrppwfhke7mi4tswqf4mbf4 -s https://api.nuget.org/v3/index.json
+
 $packPath=join-path $workspace 'src/Dnc.Events.InMemory'
 cd $packPath
 dotnet pack -c release -o bin/release
