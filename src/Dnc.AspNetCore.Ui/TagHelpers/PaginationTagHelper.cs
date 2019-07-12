1433e4d73cd6db8a6dd4f58879fe181839a2c3b6
Zj <519564415@qq.com>
2019-07-12 16:55:41 +0800

    :construction:(aspnetcore.ui:pagination tag helper)

---
+++ src/Dnc.AspNetCore.Ui/TagHelpers/PaginationTagHelper.cs
@@ -0,0 +1,10 @@
+﻿/*************************************************************************
+  PaginationTagHelper.cs:Pagination 分页器tag helper 封装
+ * **********************************************************************/
+namespace Dnc.AspNetCore.Ui.TagHelpers
+{
+    public class PaginationTagHelper
+    {
+
+    }
+}
